218ad2358b261f913c5714f6d7c270493a2f43f5
Arseni Harkunou <arseni@stillhuman.com>
2026-05-30 16:16:40 -0700

    Use webpack for Vercel builds

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ["127.0.0.1"],
-  turbopack: {
-    root: process.cwd(),
-  },
 };
 
 export default nextConfig;
